39a45a04ce1794ec9c6b968756dd529acd68bdb5
Nevidomskii <nevidomskiy8@gmail.com>
2020-08-13 23:00:02 +0300

    add alertHandel

--- src/App.js
+++ src/App.js
@@ -4,20 +4,25 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import { Home } from './pages/Home';
 import { About } from './pages/About';
 import { Profile } from './pages/Profile';
+import { Alert } from './components/Alert';
+import { AlertState } from './context/alert/alertState';
 
 
 function App() {
   return (
-    <BrowserRouter>
-      <NavBar />
-      <div className='container pt-4'>
-        <Switch>
-          <Route path='/' exact component={Home} />
-          <Route path='/about' component={About} />
-          <Route path='/profile/:name' component={Profile} />
-        </Switch>
-      </div>
-    </BrowserRouter>
+    <AlertState>
+      <BrowserRouter>
+        <NavBar />
+        <div className='container pt-4'>
+          <Alert alert={{ text: 'Test alert' }} />
+          <Switch>
+            <Route path='/' exact component={Home} />
+            <Route path='/about' component={About} />
+            <Route path='/profile/:name' component={Profile} />
+          </Switch>
+        </div>
+      </BrowserRouter>
+    </AlertState>
   );
 }
 
